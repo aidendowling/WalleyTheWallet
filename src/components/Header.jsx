61cec62d1353078d82a0e40f7adf7225e20ca090
yarak2005 <ysk5067@psu.edu>
2024-09-28 23:55:00 -0400

    Update Header.jsx
    fixed header page 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
+import{ ArrowLeft } from "lucide-react";
 const Header = ({ title, points }) => (
-    <div className="flex justify-between items-center py-6 px-8 bg-gray-100 rounded-lg mb-6">
-      <div>
-        <a href="home"> <p> &#9664; </p> </a>
-        <h1 className="text-3xl font-bold mb-2">{title}</h1>
-        <p className="text-xl font-semibold">Points: {points}</p>
+  <div className="flex justify-between items-center py-6 px-8 bg-gray-100 rounded-lg mb-6">
+    <div>
+    <div className="flex items-center space-x-2">
+  <a href="home">
+    <ArrowLeft />
+  </a>
+  <h1 className="text-3xl font-bold mb-2">{title}</h1>
+</div>
+        <p className="text-xl font-semibold"> Points: {points}</p>
       </div>
-    </div>
+      </div>
   );
 
     export default Header;
