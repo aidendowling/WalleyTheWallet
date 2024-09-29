be638d79a7909434ad25daec4bce660cc50e264b
yarak2005 <ysk5067@psu.edu>
2024-09-28 23:16:26 -0400

    Update Header.jsx
    Added an arrow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 const Header = ({ title, points }) => (
     <div className="flex justify-between items-center py-6 px-8 bg-gray-100 rounded-lg mb-6">
       <div>
+        <a href="home"> <p> &#9664; </p> </a>
         <h1 className="text-3xl font-bold mb-2">{title}</h1>
         <p className="text-xl font-semibold">Points: {points}</p>
       </div>
